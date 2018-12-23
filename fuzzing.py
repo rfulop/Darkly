709962994064036db9fe99d583eadd189bed640d
Woomb <woomb@localhost.localdomain>
2018-12-23 12:43:01 +0100

    Maj

---
+++ fuzzing.py
@@ -0,0 +1,39 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+URL = "http://192.168.1.13/.hidden/"
+
+s = ''
+
+resp = [
+        "Demande Ã  ton voisin du dessus  ",
+        "Demande Ã  ton voisin du dessous ",
+        "Demande Ã  ton voisin de gauche  ",
+        "Demande Ã  ton voisin de droite  ",
+        "Non ce n'est toujours pas bon ...",
+        "Tu veux de l'aide ? Moi aussi !  ",
+        "Toujours pas tu vas craquer non ?",
+        ]
+
+def f(s):
+    r = requests.get(URL+s)
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    l = []
+    for link in soup.find_all('a'):
+        l.append(link.get('href'))
+    l = l[1:]
+    if 'README' in l:
+        print('README on %s' % URL+s+'README')
+        r = requests.get(URL+s+'README')
+        data = r.text.replace('\n', '')
+        
+        if data not in resp:
+            print("'%s'" % r.text)
+            sys.exit()  
+
+    for e in l:
+        f(s+e)
+
+f(s)
